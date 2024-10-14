0cfa58d4da67241da221264ac404249d5722a37b
Toyman-tech <adiguno204@gmail.com>
2024-10-14 18:40:43 +0100

    lfg

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,40 @@ const dmSans = DM_Sans({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: {
     template: "%s | TOYMANDATAHUB",
-    default: "TOYMAN DATAHUB",
+    default: "Toyman DataHub - Buy Airtime, Data, Pay Bills, Cable Subscription, and more..",
   },
-  description: "TOYMAN DATAHUB IS A VTU PLATFORM FOR ALL NETWORKS",
-  icons: "/logo.svg",
+  description: "Toyman DataHub offers affordable VTU services for buying airtime, data for all networks, paying electricity bills, cable subscriptions, and scratch cards online. Fast, reliable, and secure.",
+  keywords: [
+    'Toyman data hub',
+    'Toyman datahub',
+    'Buy Airtime online',
+    'Data subscription for all networks',
+    'Pay Electricity Bills Online',
+    'Cable Subscription Payment',
+    'Scratch Card Purchase',
+    'Cheap Airtime VTU',
+    'Reliable VTU Service'
+  ],
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: { url: "/apple-touch-icon.png", sizes: "180x180" },
+  },
+  openGraph : {
+    type: "website",
+    title:'Toyman DataHub - Buy Airtime, Data, Pay Bills, Cable Subscription, and more..',
+    description: 'Get fast and secure VTU services at Toyman DataHub. Buy airtime, data, pay bills, and more!',
+    url: 'https://www.toymandatahub.com',
+    siteName: 'Toyman DataHub',
+    locale: 'en_us'
+    // images: [
+    //   {
+    //     url: ''
+    //   }
+    // ]
+  }
 };
 
 export default function RootLayout({
